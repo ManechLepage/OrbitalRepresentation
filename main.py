834b9fccb851df59e0b074173e4b8346608df895
Melkor <melkor5456@gmail.com>
2024-11-16 15:41:11 -0500

    kinetic integral

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.special import factorial
 from scipy.special import sph_harm
+from scipy import integrate
 
 def get_slater_type_orbital(n, l, m, r, theta, phi, zeta):
     return radial_part(r, n, zeta) * sph_harm(m, l, phi, theta)
@@ -18,13 +19,21 @@ def get_omega(n, l, s, zeta):
     second_part = factorial(n - s) / (factorial(s) * factorial(n - l - (2 * s)))
     return first_part * second_part
 
-def overlap_integral(n, n2, l, l2, m, m2, zeta, zeta2):
-    if l == l2:
+def overlap_integral(n0, n1, l0, l1, m0, m1, zeta0, zeta1):
+    if l0 != l1 or m0 != m1:
         return 0
-    if m == m2:
-        return 0
-    return factorial(n + n2) / ((zeta + zeta2) ** (n + n2 + 1))
+    return factorial(n0 + n1) / ((zeta0 + zeta1) ** (n0 + n1 + 1))
 
+def kinetic_integral(n0, n1, l0, l1, m0, m1, zeta0, zeta1):
+    if l0 != l1 or m0 != m1:
+        return 0
+    constant0 = l1 * (l1 + 1) - n1 * (n1 - 1)
+    constant1 = 2 * zeta1 * n1
+    constant2 = -2 * zeta1 * zeta1
+    integrand = lambda r: np.exp(-(zeta0 + zeta1) * r) * (constant0 * r ** (n0 + n1 - 2) + constant1 * r ** (n0 + n1 - 1) + constant2 * r ** (n0 + n1))
+    return integrate.quad(integrand, 0, np.inf) / 2
+    
+    
 def main():
     orbital = input()
     n = int(orbital[0])
